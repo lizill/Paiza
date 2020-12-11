a838df6e4bbd6a491b45adcc584d4896c29955d8
lizil <lizil@DESKTOP-ESUHEF2>
2020-12-11 13:30:53 +0900

    Paiza java

--- src/RankD/RankD.java
+++ src/RankD/RankD.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 public class RankD {
 
 	public static void main(String[] args) {
+//		ichimanPo();
+//		nKakuKata();
 //		printString();
 //		underLine();
 		plus();
 	}
 	
-	public static void printString() {
-		  Scanner input = new Scanner(System.in);
-		  
-	      String str1 = input.nextLine();
-	      
-	      for(int i=0; i<str1.length()-8; i++) {
-	          System.out.print(str1.charAt(i));
-	      }
-	      System.out.println("!!");
-	      
-	      input.close();
+	public static void plus() {
+		Scanner input = new Scanner(System.in);
+		
+		int v1 = input.nextInt();
+		String plus = input.next();
+		int v2 = input.nextInt();
+		
+		System.out.println(v1+v2);
+		
+		input.close();
 	}
 	
 	public static void underLine() {
@@ -36,14 +37,43 @@ public class RankD {
 		input.close();
 	}
 	
-	public static void plus() {
+	public static void printString() {
+		  Scanner input = new Scanner(System.in);
+		  
+	      String str1 = input.nextLine();
+	      
+	      for(int i=0; i<str1.length()-8; i++) {
+	          System.out.print(str1.charAt(i));
+	      }
+	      System.out.println("!!");
+	      
+	      input.close();
+	}
+	
+	public static void nKakuKata() {
+		
 		Scanner input = new Scanner(System.in);
 		
-		int v1 = input.nextInt();
-		String plus = input.next();
-		int v2 = input.nextInt();
+		int n = input.nextInt();
 		
-		System.out.println(v1+v2);
+		int naikaku = 180*(n-2)/n;
+		
+		System.out.println(naikaku);
+		
+		input.close();
+	}
+	
+	public static void ichimanPo() {
+		Scanner input = new Scanner(System.in);
+		int d = input.nextInt();
+		int s = input.nextInt();
+		int po = d*100000/s;
+		
+		if(po >= 10000) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
 		
 		input.close();
 	}
